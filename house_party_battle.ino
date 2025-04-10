f9af285c74e69a53a48e9d0f300fb6829eda0865
deadlykarrot <deadlykarrot@gmail.com>
2025-04-10 10:36:06 -0500

    Added switch statement to house_party_battle.ino

--- house_party_battle.ino
+++ house_party_battle.ino
@@ -8,25 +8,22 @@ Servo finalServo; // servo that knocks Aelwyn over at the end.
 
 // the pins for each switch, to access them more easily and change them quickly.
 // all figure pins are set to 4, until we decide which pins they actually go to.
-const int aelwynPin1 = 4; // pin for setting Aelwyn up outside
-const int adainePin1 = 4; // pin for setting Adaine up outside
+const int sisterPin1 = 4; // pin for setting Adaine and Aelwyn up outside
+const int spiritPin = 4; // pin for setting up the spirit guardians
 const int webPin = 4; // pin for Aelwyn in the web
 const int cyclePin = 4; // pin for the motorcycle on the stairs with Adaine attached
-int aelwynButt2; // value for setting Aelwyn up inside
-int adaineButt2; // value for setting Adaine up inside
+const int sisterPin2 = 4; // pin for setting Adaine and Aelwyn up inside
 const int buttPin = 4; // pin for the button at the end
 
 // the values of the pins, to detect when things are placed upon them
-int aelwynVal1; // value for setting Aelwyn up outside
-int adaineVal1; // value for setting Adaine up outside
+int sisterVal1; // value for setting Adaine and Aelwyn up outside
 int webVal; // value for Aelwyn in the web
 int cycleVal; // value for the motorcycle on the stairs with Adaine attached
-int aelwynVal2; // value for setting Aelwyn up inside
-int adaineVal2; // value for setting Adaine up inside
+int sisterVal2; // value for setting Adaine and Aelwyn up inside
 int buttVal; // value for the button at the end
 
 // light up the current scene the user must assemble
-bool lowerLit = false; // whether or not the lower floor (and motorcycle) are lit
+bool lowerLit = false; // whether or not the lower floor is lit
 bool upperLit = false; // whether or not the upper floor is lit
 const int lowerLedPin = 4; // the pin for the lower floor LED(s)
 const int upperLedPin = 4; // the pin for the lower floor LED(s)
@@ -41,6 +38,7 @@ int stage = 0; /* the current scene, as follows:
 2 - first floor is open, user must set up web, Aelwyn, motorcycle, Adaine on motorcycle
 3 - second floor is open, user must set up Adaine and Aelwyn at the end
 4 - Adaine and Aelwyn in place, user must press switch to end the sequence
+5 - sequence is done, victory music plays
 */
 void setup() {
   // put your setup code here, to run once:
@@ -50,10 +48,25 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  aelwynVal1 = digitalRead(aelwynPin1); // get the outside Aelwyn value
-  adaineVal1 = digitalRead(adainePin1); // get the outside Adaine value
+  sisterVal1 = digitalRead(sPin1); // get the outside Adaine and Aelwyn value
   webVal = digitalRead(webPin); // get the web value
   cycleVal = digitalRead(cyclePin); // get the motorcycle value
-  aelwynVal2 = digitalRead(aelwynPin2); // get the inside Aelwyn value
-  adaineVal2 = digitalRead(adainePin2); // get the inside Adaine value
+  sisterVal2 = digitalRead(sisterPin2); // get the inside Adaine and Aelwyn value
+  buttVal = digitalRead(buttPin); // get the button value
+  switch(stage){
+    case 0:
+
+    case 1:
+
+    case 2:
+
+    case 3:
+
+    case 4:
+
+    case 5:
+
+    default:
+      break;
+  }
 }
